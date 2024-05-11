2ce3142bfc3f3ab2788daac990d00cdbbf8d1cd2
visheshkakadiya <jjkakadiya01@gmail.com>
2024-05-11 11:36:13 +0530

    Add comment router

---
+++ src/routes/comment.routes.js
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import { verifyJWT } from "../middlewares/auth.middleware";
+import { upload } from "../middlewares/multer.middleware";
+import { addComment, deleteComment, getVideoComments, updateComment } from "../controllers/comment.controller";
+
+const router = Router();
+
+router.use(verifyJWT, upload.none()); // Apply verifyJWT middleware to all routes in this file
+
+router.route("/:videoId").get(getVideoComments).post(addComment)
+router.route("/c/:commentId").delete(deleteComment).patch(updateComment)
+
+export default router;
