f0e0d1fad9399f379bd5afe2963940a8a5bcf3bc
visheshkakadiya <jjkakadiya01@gmail.com>
2024-05-17 11:14:17 +0530

    Add dahboard, healthcheck and like routes

---
+++ src/routes/dashboard.routes.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import { verifyJWT } from "../middlewares/auth.middleware";
+import { getChannelStats, getChannelVideos } from "../controllers/dashboard.controller";
+
+const router = Router();
+
+router.use(verifyJWT)
+
+router.route("/stats").get(getChannelStats)
+router.route("/videos").get(getChannelVideos)
+
+export default router
