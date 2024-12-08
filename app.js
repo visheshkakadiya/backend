6439670e22c7caf59b55202d5738bc10ba45c412
visheshkakadiya <jjkakadiya01@gmail.com>
2024-12-08 15:53:26 +0530

    morgan change

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({extended: true, limit: "50mb"}))
 
 app.use(express.static("temp"))
 app.use(cookieParser())
-app.use(morgan("dev"));
+app.use(morgan("start"));
 
 
 import userRouter from "./routes/user.routes.js";
