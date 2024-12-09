c1a26b2c276a4df12ea917c14a18bc1cf2990e43
visheshkakadiya <jjkakadiya01@gmail.com>
2024-12-09 20:44:45 +0530

    typos mistake on mogo_uri

--- src/DB/index.js
+++ src/DB/index.js
@@ -3,7 +3,7 @@ import { DB_name } from "../../src/constants.js";
 
 const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGO_URi}/${DB_name}`)
+        const connectionInstance = await mongoose.connect(`${process.env.MONGO_URI}/${DB_name}`)
         console.log(`\n MONGODB connected !! ${connectionInstance.connection.host}`)
     } catch (error) {
         console.log("failed to connect with MONGODB", error)
