c0eeca74e01429b802144adec5c760e636f7016e
visheshkakadiya <jjkakadiya01@gmail.com>
2024-12-11 10:15:19 +0530

    fix same file issue

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,12 +1,14 @@
 import multer from "multer";
+import { v4 as uuidv4 } from "uuid"; // To generate unique IDs for file names
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, './public/temp')
-    },
-    filename: function (req, file, cb) {
-      cb(null, file.originalname)
-    }
-  })
-  
-export const upload = multer({ storage,})
+  destination: function (req, file, cb) {
+    cb(null, './public/temp');
+  },
+  filename: function (req, file, cb) {
+    const uniqueName = `${uuidv4()}-${file.originalname}`;
+    cb(null, uniqueName);
+  }
+});
+
+export const upload = multer({ storage });
